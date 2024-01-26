3ee3f890080ef3b748d43ef8c1fa6ca0f777f40b
matze328 <152269645+matze328@users.noreply.github.com>
2024-01-26 15:58:03 +0100

    new

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('button1').addEventListener('click', function() {
+    document.getElementById('image1').src = 'bild1.jpg';
+  });
+  
+  document.getElementById('button2').addEventListener('click', function() {
+    document.getElementById('image2').src = 'bild2.jpg';
+  });
+  
+  document.getElementById('button3').addEventListener('click', function() {
+    document.getElementById('image3').src = 'bild3.jpg';
+  });
